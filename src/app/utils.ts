8a5f13bfc6bafde23c70a19b36be11af42dc8b18
Ahmed Moneim <ahnedmoneimhacher26@gmail.com>
2026-04-02 17:01:30 +0200

    fixing error of geting the token in production phese else

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -2,21 +2,26 @@ import next from "next";
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-export async function decodeAuthanticationUserToken():Promise<{token: string | undefined, userId: string | undefined} | null>{
-    const cookie = await cookies() ;
-    const nextAuthToken = cookie.get('next-auth.session-token')?.value  || cookie.get("next-auth.session-token")?.value;
+export async function decodeAuthanticationUserToken(): Promise<{
+  token: string | undefined;
+  userId: string | undefined;
+} | null> {
+  const cookie = await cookies();
+  // const nextAuthToken = cookie.get('next-auth.session-token')?.value  || cookie.get("next-auth.session-token")?.value;
+  const nextAuthToken =
+    cookie.get("__Secure-next-auth.session-token")?.value ||
+    cookie.get("next-auth.session-token")?.value;
 
-    const jwtRes = await decode({secret:process.env.NEXTAUTH_SECRET! , token:nextAuthToken})
+  const jwtRes = await decode({
+    secret: process.env.NEXTAUTH_SECRET!,
+    token: nextAuthToken,
+  });
 
-    if(jwtRes){
-        console.log('da el user id ya 3am',jwtRes.id);
-        
-        return {token:jwtRes.routeToken  ,userId: jwtRes.id}
-    }
-    else{
-        return null
-    }
+  if (jwtRes) {
+    console.log("da el user id ya 3am", jwtRes.id);
 
-
+    return { token: jwtRes.routeToken, userId: jwtRes.id };
+  } else {
+    return null;
+  }
 }
-
